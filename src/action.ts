2cf28c6f4f269f56796befa6425ba73270c3a3d6
pt-br <ltavares@yieldstreet.com>
2020-06-29 16:20:26 -0300

    Do not checkout before changing branches

--- src/action.ts
+++ src/action.ts
@@ -120,7 +120,6 @@ export async function run() {
 
       // Get base branch coverage (previous coverage)
       if (baseBranch) {
-        await exec("git checkout .", [], {})
         await exec("git checkout origin/" + baseBranch, [], {})
 
         coverageHeaderPrev = "**" + baseBranch + " coverage**\n\n"
