93ff18f0b51520c2903ba4c5e3d8532a4289e780
pt-br <ltavares@yieldstreet.com>
2020-09-22 13:23:46 -0300

    WIP: Thrown error when tests fail

--- src/action.ts
+++ src/action.ts
@@ -347,7 +347,7 @@ export async function run() {
         }
 
         if (!results.success) {
-          //core.setFailed("Some jest tests failed.")
+          core.setFailed("Some jest tests failed.")
         }
       }
     }
